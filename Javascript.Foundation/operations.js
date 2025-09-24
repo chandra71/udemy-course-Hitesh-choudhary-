358d39a56e00010cc9352cd2969e98e6d50db264
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-09-24 23:52:20 +0530

    Add files via upload

---
+++ Javascript.Foundation/operations.js
@@ -0,0 +1,39 @@
+let score = 102;
+let bonus = 25;
+
+let totalscore = score + bonus;
+
+let addition = 4 + 5;
+let subtract = 9 - 3;
+let multiply = 3 * 5;
+let division = 8 / 2;
+let reminder = 9 % 2;
+let exponent = 2 ** 4;
+
+let myscore = 110;
+myscore++;
+
+let credits = 56;
+credits--;
+
+console.log(score);
+console.log(bonus);
+console.log(totalscore);
+console.log(addition);
+console.log(subtract);
+console.log(multiply);
+console.log(division);
+console.log(reminder);
+console.log(exponent);
+console.log(myscore);
+console.log(credits);
+
+//comparison operation
+
+let num1 = 3;
+let num2 = 5;
+let num3 = 3;
+
+console.log(num1 == num2);
+console.log(num1 == num3);
+console.log(num1 != num2);
