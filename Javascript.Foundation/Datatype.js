4172fc2d86835d35f45755cf013836ed7a65089e
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-09-24 23:51:51 +0530

    Add files via upload

---
+++ Javascript.Foundation/Datatype.js
@@ -0,0 +1,26 @@
+console.log("Hello world");
+
+/*
+
+string 
+Number
+Boolean
+Bigint
+
+Undefined
+Null 
+
+Object
+
+Symbol
+
+
+*/
+
+//var score = 102
+
+let score = 102;
+let name = "chandu";
+let isLoggedin = false;
+
+console.log(isLoggedin);
