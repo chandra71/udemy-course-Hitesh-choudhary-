e7f9aab27b67318db70692b8fe4b7c190b4d70fe
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-09-24 23:52:06 +0530

    Add files via upload

---
+++ Javascript.Foundation/Logical.js
@@ -0,0 +1,13 @@
+// &&  and
+// ||  or
+// !  reverse
+
+let isloggedin = true;
+let ispaid = false;
+
+console.log(isloggedin && ispaid);
+
+let isEmailuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
