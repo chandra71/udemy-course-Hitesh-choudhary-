25e1fbff6b06cf222bba38c2164464cfa0209772
chandu_116 <118456799+chandra71@users.noreply.github.com>
2025-10-04 01:47:24 +0530

    Update loopchallenges.js

--- Javascript part4/loopchallenges.js
+++ Javascript part4/loopchallenges.js
@@ -1,21 +1,21 @@
 //1
 //calculate the sum of all numbers from 1 to 5
 
-// let sum = 0;
+ let sum = 0;
 
-// let i = 1;
+let i = 1;
 
-// while (i <= 5) {
-//   sum = sum + i;
-//   i++;
-// }
+ while (i <= 5) {
+   sum = sum + i;
+   i++;
+ }
 
-// console.log(sum);
+ console.log(sum);
 
 //2
 //counts down from 5 to 1 and stores the numbers in an array named countdown
 
-/*let countdown = [];
+let countdown = [];
 let j = 5;
 while (j > 0) {
   countdown.push(j);
@@ -39,25 +39,25 @@ do {
 //4
 //do while loop that adds numbers from 1 to 3 and stores in a var named total
 
-// let total = 0;
-// let k = 1;
-// do {
-//   total += k;
-//   k++;
-// } while (k <= 3);
-// console.log(total);
+ let total = 0;
+ let k = 1;
+ do {
+   total += k;
+   k++;
+ } while (k <= 3);
+ console.log(total);
 
 //5
 //For loop that multiplies each element in the array [2,4,6] by 2 and stores the results in a new array named mulitpliedNumbers
 
-// let mulitpliedNumbers = [];
-// let numbers = [2, 4, 6];
+ let mulitpliedNumbers = [];
+ let numbers = [2, 4, 6];
 
-// for (let i = 0; i < numbers.length; i++) {
-//   takenumber = numbers[i] * 2;
-//   mulitpliedNumbers.push(takenumber);
-// }
-// console.log(mulitpliedNumbers);
+ for (let i = 0; i < numbers.length; i++) {
+   takenumber = numbers[i] * 2;
+   mulitpliedNumbers.push(takenumber);
+ }
+ console.log(mulitpliedNumbers);
 
 //6
 //For loop thta lists all the citites in the array and stores each city in a new array named citylist
@@ -70,3 +70,4 @@ for (let c = 0; c < cities.length; c++) {
   ciytlist.push(mycity);
 }
 console.log(ciytlist);
+
